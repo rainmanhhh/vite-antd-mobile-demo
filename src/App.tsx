f76ff962c8b80be51556b22707557b12a31c08ac
huangwl <410488219@qq.com>
2021-02-01 17:05:30 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import {InputItem, List} from 'antd-mobile'
+
+function App() {
+
+  return (
+    <div className="App">
+      <p>this is app</p>
+      <List>
+        <InputItem>inputItem</InputItem>
+      </List>
+    </div>
+  )
+}
+
+export default App
